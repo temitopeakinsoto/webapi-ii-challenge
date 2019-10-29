a9be0a940d9d4e16d85e94faa391ebf6b4ca3ca3
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-29 15:22:56 +0100

    Add a POST route to postRoutes.js
    In this commit, I added a POST route which basically inserts a new blog post to the backend  and returns the id of the newly created post

--- Routes/postRoutes.js
+++ Routes/postRoutes.js
@@ -1,28 +1,49 @@
-const express = require('express');
-const db = require('../data/db');
+const express = require("express");
+const db = require("../data/db");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    db.find()
-      .then(posts => {
-        res.status(200).json(posts);
-      })
-      .catch(() => {
-        res
-          .status(500)
-          .json({
-            errorMessage: "There was an error retrieving records from the database..."
-          });
+router.get("/", (req, res) => {
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage:
+          "There was an error retrieving records from the database..."
       });
-  })
-
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(200).send(`hello from GET /post/${id} route`);
+    });
 });
 
-router.post('/', (req, res) => {
-    res.status(200).send("hello from POST /post route");
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({
+        errorMessage: "Please provide title and contents for the post."
+      });
+  } else {
+    const newBlogPost = {
+      title,
+      contents
+    };
+    db.insert(newBlogPost)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage:
+            "There was an error inserting this post to the database..."
+        });
+      });
+  }
+});
+
+router.post("/", (req, res) => {
+  res.status(200).send("hello from POST /post route");
 });
 
 module.exports = router;
