b0623e959759cf4768c8ccd4f7293774c4aac1c4
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-29 13:03:06 +0100

    Initialize project and create barebone api server file

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("server running on port " + (process.env.PORT || 4000));
+});
